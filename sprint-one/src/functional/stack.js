e06601a7649ec6d2582b53466d3f3bdd3c26d604
misteryeo <andy.yeo@gmail.com>
2017-02-08 14:43:35 -0800

    Completed stack.js for functional instantiation class.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,31 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0; 
+  var popcounter = 0;
+  var pushcounter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value; 
+    pushcounter++; 
+    size++;
   };
 
   someInstance.pop = function() {
+    popcounter++;
+    size--;
+    var value = storage[size]; 
+    delete storage[size]; 
+    return value; 
   };
 
   someInstance.size = function() {
+    if (popcounter > pushcounter) {
+      return 0; 
+    } else {
+      return size;
+    }
   };
 
   return someInstance;
