8af59bb1c59715468044dbb185641a75d74a55dd
misteryeo <andy.yeo@gmail.com>
2017-02-09 11:40:08 -0800

    Completed first three test of linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,12 +1,27 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
+  list.head = null; //
   list.tail = null;
+  // _.extend(list, NodeFunction); //is NodeFunction getting extended 
+  
 
   list.addToTail = function(value) {
+    var tailNode = NodeFunction(value);
+    if (list.head == null && list.tail == null) {
+      list.head = tailNode;
+      list.tail = tailNode;
+    } else {
+      list.tail = tailNode.next; 
+    }
+
+    // list.tail = {}; //list.tail was null. making {} 
+    // list.tail.value = value; //setting tail.value = value
   };
 
-  list.removeHead = function() {
+  list.removeHead = function() { 
+    // var delHead = list.head;
+    // list.head = this.next;
+    // delete list.head;
   };
 
   list.contains = function(target) {
@@ -15,7 +30,7 @@ var LinkedList = function() {
   return list;
 };
 
-var Node = function(value) {
+var NodeFunction = function(value) {
   var node = {};
 
   node.value = value;
