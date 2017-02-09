d7b1aeeced580d24b028c3ace39b95e2407900a9
misteryeo <andy.yeo@gmail.com>
2017-02-08 21:13:38 -0800

    Completed queue.js for protoypal instantiation pattern.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  instance.count = 0;
+  instance.addCount = 0;
+  instance.deleteCount = 0;
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.storage[this.addCount] = value;
+  this.count++;
+  this.addCount++;
+};
 
+queueMethods.dequeue = function () {
+  var value = this.storage[this.deleteCount];
+  this.deleteCount++;
+  if (this.count > 0) {
+    this.count--;
+  }
+  return value;
+};
+
+queueMethods.size = function () {
+  return this.count;
+};
