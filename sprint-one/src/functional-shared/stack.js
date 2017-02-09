2ac1680ab719a1c30f987142e19d0d39c939dccd
misteryeo <andy.yeo@gmail.com>
2017-02-08 16:43:02 -0800

    Half way through stack.js for functional-shared instantiation pattern.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
+var stackMethods = {
+  push: function (value) {
+    storage[size] = value;
+    size++;
+  },
+  pop: function () {
+    if (size > 0) {
+      size--;
+    }
+    var value = storage[size];
+    delete storage[size];
+    return value;
+  },
+  size: function () {
+    return size;
+  }
+};
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {}; 
+  var storage = {}; 
+  var size = 0;
+
+  instance.push = stackMethods.push;
+
+  instance.pop = stackMethods.pop;
+
+  instance.size = stackMethods.size;
+
+  return instance;
 };
 
-var stackMethods = {};
+
 
 
