e086cfec23575a29a431e85fdf4c697f98f705d0
misteryeo <andy.yeo@gmail.com>
2017-02-10 14:20:50 -0800

    Passed 'should remove edges between nodes' test

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -54,12 +54,21 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  // 
   this.edge.push([fromNode, toNode]);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var removedEdge = null;
+  // Iterate through the edge array for each nested array
+  for (var i = 0; i < this.edge.length; i++) {
+    // Check if the nested array indices match the arguments passed in
+    if ((this.edge[i][0] === fromNode && this.edge[i][1] === toNode) ||
+      (this.edge[i][1] === fromNode && this.edge[i][0] === toNode)) {
+      removedEdge = this.edge.splice(i); 
+    }
+  }
+  return removedEdge;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
