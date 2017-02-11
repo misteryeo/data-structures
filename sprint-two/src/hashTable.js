121e20afde4bc1809d890afb57beef7a11b628b5
misteryeo <andy.yeo@gmail.com>
2017-02-11 09:26:32 -0800

    Completed 'should not contain values that were removed' test in HashTab.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -23,6 +23,14 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i] === k) {
+      bucket.splice(i, 1);
+    }
+  }
+
 };
 
 
