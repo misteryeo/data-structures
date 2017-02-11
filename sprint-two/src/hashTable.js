835b05c7d821b41b544d92b723bd3b41c70ced2f
misteryeo <andy.yeo@gmail.com>
2017-02-11 09:22:01 -0800

    Completed 'should not contain values that were not inserted' & 'should overwrite values that have the same key' tests in hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,32 +5,20 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit); // gives back index for where it will go
-  // Check if storage bucket is undefined
-  // if (this._storage[index] === undefined) {
-  // // If so, establish a storage bucket
-  //   this._storage[index] = [];
-  // }
-  // Otherwise, push the string value pair into storage bucket
-  this._storage.set(index, [k, v]);
+  var tuple = [k, v];
+  this._storage.set(index, tuple);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // for (var i = 0; i < this._storage[index]; i++) {
-  //   if (this._storage[index][i][0] === k) {
-  //     console.log('hello');
-  //     return this._storage[index][i][1];
-  //   }
-  // }
   
   var bucket = this._storage.get(index);
-  
-  
-  bucket.each(function(item) {
-    return item[1];
-  });
 
-
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i] === k) {
+      return bucket[i + 1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
