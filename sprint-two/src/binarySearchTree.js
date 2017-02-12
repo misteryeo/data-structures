0357f43ba50084fc44436360287b4cf1fafe4855
Andy Yeo <andy.yeo@gmail.com>
2017-02-12 07:38:33 -0800

    Passed 'should insert values at the correct location in the tree' and 'should have a working "contains" method' tests for binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -10,11 +10,31 @@ var BinarySearchTree = function(value) {
 var binaryTreeMethod = {};
 
 binaryTreeMethod.insert = function(value) {
-
+  var tree = BinarySearchTree(value)
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = tree;
+    } else {
+      this.left.insert(value);
+    }
+  } else if (value > this.value) {
+    if (this.right === null) {
+      this.right = tree;
+    } else {
+      this.right.insert(value);
+    }
+  } 
 };
 
 binaryTreeMethod.contains = function(value) {
-
+  var tree = BinarySearchTree(value)
+    if (value === this.value) {
+      return true;
+  } else if (value < this.value) {
+      return !!(this.left && this.left.contains(value));
+  } else if (value > this.value) {
+      return !!(this.right && this.right.contains(value));
+  }
 };
 
 binaryTreeMethod.depthFirstLog = function(callBack) {
