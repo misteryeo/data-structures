a06e00a8e75de9fa3d53fb191967a8e587543879
Andy Yeo <andy.yeo@gmail.com>
2017-02-12 07:23:42 -0800

    Passed 'should have methods' test in binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,22 @@
 var BinarySearchTree = function(value) {
+
+  var binaryTree = Object.create(binaryTreeMethod);
+  binaryTree.value = value;
+  binaryTree.left = null;
+  binaryTree.right = null;
+  return binaryTree;
+  };
+
+var binaryTreeMethod = {};
+
+binaryTreeMethod.insert = function(value) {
+
 };
 
+binaryTreeMethod.contains = function(value) {
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+};
+
+binaryTreeMethod.depthFirstLog = function(callBack) {
+
+};
